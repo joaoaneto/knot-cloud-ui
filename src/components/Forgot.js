e572b6fa17e4b7c54273aa267e0a1867fa794c8d
Paulo Serra Filho <ptvsf@cesar.org.br>
2018-10-25 18:37:31 -0300

    Create forgot password front-end
    Change-Id: If50f133782b0792ba688597a15975b2ff31c0104
    Reviewed-on: https://review.gerrithub.io/430862
    Reviewed-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>
    Tested-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>

--- src/components/Forgot.js
+++ src/components/Forgot.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import '../styles/Forgot.css';
+import { Link } from 'react-router-dom';
 
 class Forgot extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    const { email } = this.state;
+
+    window.alert(`Reset e-mail sent to ${email}`); // eslint-disable-line no-alert
+    e.preventDefault();
+  }
+
   render() {
     return (
-      <div>
-        <h1>Forgot password page</h1>
+      <div className="forgot-pwd-wrapper">
+        <h3 className="page-title"> FORGOT PASSWORD </h3>
+        <form className="reset-form" onSubmit={e => this.handleSubmit(e)}>
+          <input type="email" className="text-input" id="reset-user-email" placeholder="Email" onChange={e => this.setState({ email: e.target.value })} required />
+          <br />
+          <input type="submit" className="btn btn-primary" id="reset-user-btn" value="SUBMIT" />
+          <br />
+        </form>
+        <Link to="/">
+          <button className="btn btn-secondary" type="button">
+            LOGIN
+          </button>
+        </Link>
       </div>
     );
   }
