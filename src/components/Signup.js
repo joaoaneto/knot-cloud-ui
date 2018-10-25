3e275c5e9482fd3a28a1442683454192e6aa91c0
Ramon Ribeiro <rhpr@cesar.org.br>
2018-10-25 12:49:24 -0300

    Create sign up front-end
    Change-Id: Ib1bbdc5e2f086e6d34d92151bd0398cd86a24cf8
    Reviewed-on: https://review.gerrithub.io/430840
    Reviewed-by: Paulo Serra Filho <ptvsf@cesar.org.br>
    Reviewed-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>
    Tested-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignup = this.handleSignup.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.id]: e.target.value });
+  }
+
+  handleSignup(e) {
+    const { email, password, confirmPassword } = this.state;
+    e.preventDefault();
+    // TODO: Request to authenticator
+    alert(`${email}\n${password}\n${confirmPassword}`);
+  }
+
   render() {
     return (
-      <div>
-        <h1>Sign-up page</h1>
+      <div className="signup">
+        <form onSubmit={e => this.handleSignup(e)}>
+          <input className="text-input" id="email" type="email" onChange={this.handleChange} placeholder="Email" required />
+          <br />
+          <input className="text-input" id="password" type="password" onChange={this.handleChange} placeholder="Password" required />
+          <br />
+          <input className="text-input" id="confirmPassword" type="password" onChange={this.handleChange} placeholder="Confirm Password" required />
+          <br />
+          <input className="btn btn-primary" id="button" type="submit" value="Sign up" />
+        </form>
+        <Link to="/">
+          <input className="btn btn-secondary" id="button-signin" type="submit" value="Sign in" />
+        </Link>
       </div>
     );
   }
