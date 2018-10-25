44fedb00822917f0b8f828c29aa2e8485eda5c8c
Ramon Ribeiro <rhpr@cesar.org.br>
2018-10-25 14:27:01 -0300

    Create sign in front-end
    Change-Id: Ifd40ad19c620c00ebcb88aa158efda4a5a74dc36
    Reviewed-on: https://review.gerrithub.io/430841
    Reviewed-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>
    Tested-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Signin.css';
 
 class Signin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignin = this.handleSignin.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.id]: e.target.value });
+  }
+
+  handleSignin(e) {
+    const { email, password } = this.state;
+    e.preventDefault();
+    // TODO: Request to authenticator
+    alert(`${email}\n${password}`);
+  }
+
   render() {
     return (
-      <div>
-        <h1>Sign-in page</h1>
+      <div className="Signin">
+        <form onSubmit={e => this.handleSignin(e)}>
+          <input className="text-input" id="email" type="email" onChange={this.handleChange} placeholder="Email" required />
+          <br />
+          <input className="text-input" id="password" type="password" onChange={this.handleChange} placeholder="Password" required />
+          <br />
+          <input className="btn btn-primary" id="button-signin" type="submit" value="Sign in" />
+          <br />
+        </form>
+        <Link to="/signup">
+          <input className="btn btn-secondary btn-small" id="button-signup" type="submit" value="Sign up" />
+        </Link>
+        <Link to="/forgot">
+          <input className="btn btn-secondary btn-small" id="button-forgot" href="/forgot" type="submit" value="Forgot password?" />
+        </Link>
       </div>
     );
   }
