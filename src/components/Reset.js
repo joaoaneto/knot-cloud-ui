a84ade6c3124f860063aa67846d76006e3036d3b
Paulo Serra Filho <ptvsf@cesar.org.br>
2018-10-26 18:57:44 -0300

    Create reset password front-end
    Change-Id: Ice403985b013e4cec8b38b434f08adcb427d75a5
    Reviewed-on: https://review.gerrithub.io/430863
    Reviewed-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>
    Tested-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
+import '../styles/Reset.css';
+import { Redirect } from 'react-router-dom';
 
 class Reset extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    const { password } = this.state;
+    const { confirm } = this.state;
+
+    if (password !== confirm) {
+      window.alert("Passwords don't match!"); // eslint-disable-line no-alert
+    } else {
+      window.alert('Password updated!'); // eslint-disable-line no-alert
+      this.setState({
+        redirect: true
+      });
+    }
+    e.preventDefault();
+  }
+
+  renderRedirect() { // eslint-disable-line consistent-return
+    const { redirect } = this.state;
+
+    if (redirect) {
+      return <Redirect to="/" />;
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Reset password page</h1>
+      <div className="reset-pwd-wrapper">
+        <h3 className="page-title"> RESET PASSWORD </h3>
+        <form className="reset-form" onSubmit={e => this.handleSubmit(e)}>
+          <input type="password" className="text-input" id="new-password" placeholder="New Password" onChange={e => this.setState({ password: e.target.value })} required />
+          <br />
+          <input type="password" className="text-input" id="new-password-confirm" placeholder="Confirm new password" onChange={e => this.setState({ confirm: e.target.value })} required />
+          <br />
+          <input type="submit" className="btn btn-primary" id="reset-user-btn" value="SUBMIT" />
+          <br />
+        </form>
+        {this.renderRedirect()}
       </div>
     );
   }
