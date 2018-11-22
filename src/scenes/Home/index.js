ed09ac656502c23d8509e9a90f86324e6e3e9497
Ramon Ribeiro <rhpr@cesar.org.br>
2018-11-22 12:58:26 -0300

    Render Cardboard in Home scene
    Change-Id: I523d68df7e22c76973c1ad19484ac4ef333c6df1

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import AddButton from './components/AddButton';
+import CardBoard from './components/CardBoard';
+import Card from './components/Card';
 import './styles.css';
 
 class Home extends Component {
@@ -9,6 +11,8 @@ class Home extends Component {
     super(props);
     this.state = {
       currentScene: 'Gateways',
+      gatewaysList: [], // TODO: Initialize thoses lists with service getDevices
+      appsList: [],
       redirect: false
     };
     this.updateCurrentScente = this.updateCurrentScente.bind(this);
@@ -30,21 +34,102 @@ class Home extends Component {
     });
   }
 
-  addDevice() {
-    const { currentScene } = this.state;
+  updateInCloud(uuid, title, content) {
+    const { currentScene, gatewaysList, appsList } = this.state;
+    const list = currentScene === 'Gateways' ? gatewaysList : appsList;
+    const device = list.find(dev => dev.uuid === uuid);
+    device[title] = content;
+    // TODO: request to service
+    if (currentScene === 'Gateways') {
+      this.setState({ gatewaysList: list });
+    } else if (currentScene === 'Apps') {
+      this.setState({ appsList: list });
+    }
+  }
 
-    window.alert(`Add new device on ${currentScene}`); // eslint-disable-line no-alert
+  removeFromCloud(uuid) {
+    const { currentScene, gatewaysList, appsList } = this.state;
+    const list = currentScene === 'Gateways' ? gatewaysList : appsList;
+
+    // TODO: request to service
+    list.splice(list.findIndex(i => i.uuid === uuid), 1);
+    if (currentScene === 'Gateways') {
+      this.setState({ gatewaysList: list });
+    } else if (currentScene === 'Apps') {
+      this.setState({ appsList: list });
+    }
+  }
+
+  addInCloud(objModal) {
+    // TODO: request to service
+    const device = {
+      uuid: Math.floor(Math.random() * 10000),
+      token: 'secret'
+    };
+    const { currentScene, gatewaysList, appsList } = this.state;
+    const list = currentScene === 'Gateways' ? gatewaysList : appsList;
+
+    Object.assign(device, objModal); // merge objects
+    list.push(device);
+    if (currentScene === 'Gateways') {
+      this.setState({ gatewaysList: list });
+    } else if (currentScene === 'Apps') {
+      this.setState({ appsList: list });
+    }
+  }
+
+  addDevice() {
+    // TODO: objModal comes from the modal callback
+    const objModal = {
+      name: 'Raspberry1'
+    };
+
+    this.addInCloud(objModal);
+  }
+
+  showCards(list) {
+    const cards = [];
+    let body;
+    let header;
+    list.forEach((device) => {
+      const fieldsToHide = ['uuid', 'token', 'name'];
+      if (device.name) {
+        header = device.name;
+      }
+      body = Object.keys(device).map(i => ({ title: i, content: `${device[i]}`, isHidden: false }));
+      body.forEach((i) => { // Hide fields in body
+        if (fieldsToHide.includes(i.title)) {
+          i.isHidden = true;
+        }
+      });
+      cards.push(<Card
+        header={header}
+        onHeaderChange={(headerChange) => { this.updateInCloud(device.uuid, 'name', headerChange); }}
+        body={body}
+        onBodyChange={(title, content) => this.updateInCloud(device.uuid, title, content)}
+        action={{ click: () => { this.removeFromCloud(device.uuid); }, icon: 'delete' }}
+        key={`${device.uuid}`}
+        id={`${device.uuid}`}
+      />);
+    });
+    return cards;
   }
 
   showRegisteredDevices() {
-    const { currentScene } = this.state;
+    const { currentScene, gatewaysList, appsList } = this.state;
 
     switch (currentScene) {
       case 'Apps':
-        return (<div> Apps </div>);
+        return (
+          <CardBoard>
+            {this.showCards(appsList)}
+          </CardBoard>);
       case 'Gateways':
       default:
-        return (<div> Gateways </div>);
+        return (
+          <CardBoard>
+            {this.showCards(gatewaysList)}
+          </CardBoard>);
     }
   }
 
