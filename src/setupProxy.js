7a47c8e8513a8807c9f63445651b1bfbf355d1d5
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-11-27 17:56:31 -0300

    Change configuration key name
    API_HOST -> API_HOSTNAME. API_HOST will continue to be supported until
    the next major release.
    
    Change-Id: I74c22269fa97865672cf3f15f7fc8b2e34a06891
    Reviewed-on: https://review.gerrithub.io/435174
    Reviewed-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>
    Tested-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = (app) => {
-  const host = process.env.API_HOST || 'localhost';
+  const host = process.env.API_HOSTNAME || process.env.API_HOST || 'localhost';
   const port = process.env.API_PORT || 3003;
   const target = `http://${host}:${port}`;
   app.use(proxy('/api', {
