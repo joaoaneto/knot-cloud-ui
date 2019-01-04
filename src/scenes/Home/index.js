5436ae68b43041fd0c3324b5508c865a10c002b5
Paulo Serra Filho <ptvsf@cesar.org.br>
2019-01-04 08:36:01 -0300

    Register a device on cloud
    When the add button is pressed a modal dialog opens
    and requests a name to register a gateway/app.
    
    Co-authored-by: Ramon Ribeiro <rhpr@cesar.org.br>
    
    Change-Id: Ief28712dfbc28c97e05ccb35114a7f4d841ba892
    Reviewed-on: https://review.gerrithub.io/c/439751
    Reviewed-by: Paulo Serra Filho <ptvsf@cesar.org.br>
    Tested-by: Paulo Serra Filho <ptvsf@cesar.org.br>

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,22 +1,52 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import Storage from 'services/Storage';
+import ErrorMessage from 'components/ErrorMessage';
+import Cloud from './services/Cloud';
 import Navbar from './components/Navbar';
 import AddButton from './components/AddButton';
+import CardBoard from './components/CardBoard';
+import DeviceCard from './components/DeviceCard';
+import Modal from './components/Modal';
 import './styles.css';
 
 const actions = ['Gateways', 'Apps', 'Sign Out'];
+const { uuid: userUuid, token: userToken } = Storage.getCredentials();
+const wsHostname = process.env.WS_HOSTNAME || 'localhost';
+const wsPort = process.env.WS_PORT || 3004;
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      cloud: new Cloud(wsHostname, wsPort, userUuid, userToken),
       currentScene: 'Gateways',
-      redirect: false
+      appsList: [],
+      gatewaysList: [],
+      errorMessage: '',
+      redirect: false,
+      showModal: false
     };
     this.updateCurrentScene = this.updateCurrentScene.bind(this);
     this.signout = this.signout.bind(this);
     this.addDevice = this.addDevice.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  async componentDidMount() {
+    const { cloud } = this.state;
+
+    try {
+      await cloud.connect();
+      // TODO: initialize lists with devices on cloud
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+  }
+
+  componentWillUnmount() {
+    const { cloud } = this.state;
+    cloud.close();
   }
 
   updateCurrentScene(newScene) {
@@ -33,26 +63,80 @@ class Home extends Component {
     });
   }
 
-  addDevice() {
-    const { currentScene } = this.state;
+  async addDevice(newDeviceName) {
+    const {
+      currentScene, cloud, gatewaysList, appsList
+    } = this.state;
+    this.setState({ errorMessage: '' });
+    const type = currentScene === 'Gateways' ? 'gateway' : 'app';
+    const list = currentScene === 'Gateways' ? gatewaysList : appsList;
+    const name = newDeviceName;
 
-    window.alert(`Add new device on ${currentScene}`); // eslint-disable-line no-alert
+    try {
+      const device = await cloud.register({ type, name });
+      list.push(device);
+      if (type === 'gateway') {
+        this.setState({ gatewaysList: list });
+      } else if (type === 'app') {
+        this.setState({ appsList: list });
+      }
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+    this.toggleModal();
+  }
+
+  toggleModal() {
+    const { showModal } = this.state;
+    this.setState({
+      showModal: !showModal
+    });
+  }
+
+  async updateOnCloud(device, key, value) {
+    // TODO: make request `updateMetada` to cloud
+    console.log(`device ${device.uuid} change property ${key} to ${value}`); // eslint-disable-line no-console
+  }
+
+  async deleteOnCloud(uuid) {
+    // TODO: make request `unregister` to cloud
+    console.log(uuid); // eslint-disable-line no-console
+  }
+
+  async createSessionTokenOnCloud(device) {
+    // TODO: make request `createSessionToken` to cloud
+    device.token = 'new token';
+    return device; // This return is required as a device where should have the new session token
+  }
+
+  showCards(list) {
+    return list.map(device => (
+      <DeviceCard
+        key={device.uuid}
+        device={device}
+        onPropertyChange={(property, content) => this.updateOnCloud(device, property, content)}
+        onDelete={() => this.deleteOnCloud(device.uuid)}
+        onDownload={() => this.createSessionTokenOnCloud(device)}
+      />
+    ));
   }
 
   showCurrentScene() {
-    const { currentScene } = this.state;
+    const { currentScene, gatewaysList, appsList } = this.state;
 
     switch (currentScene) {
       case 'Apps':
-        return (<div> Apps </div>);
+        return (<CardBoard>{this.showCards(appsList)}</CardBoard>);
       case 'Gateways':
       default:
-        return (<div> Gateways </div>);
+        return (<CardBoard>{this.showCards(gatewaysList)}</CardBoard>);
     }
   }
 
   render() {
-    const { currentScene, redirect } = this.state;
+    const {
+      currentScene, redirect, showModal, errorMessage
+    } = this.state;
 
     return (
       <div className="home-wrapper">
@@ -64,7 +148,15 @@ class Home extends Component {
             else this.updateCurrentScene(action);
           }}
         />
-        <AddButton onClick={this.addDevice} />
+        {errorMessage && <ErrorMessage message={errorMessage} />}
+        <AddButton onClick={this.toggleModal} />
+        {showModal
+        && (
+        <Modal
+          currentScene={currentScene}
+          onCloseRequest={this.toggleModal}
+          onSaveDevice={this.addDevice}
+        />)}
         {this.showCurrentScene()}
         {redirect && <Redirect to="/signin" />}
       </div>
--- src/scenes/Home/services/Cloud/index.js
+++ src/scenes/Home/services/Cloud/index.js
@@ -9,6 +9,39 @@ class Cloud {
       token
     });
   }
+
+  connect() {
+    return new Promise((resolve, reject) => {
+      this.client.connect();
+      this.client.once('ready', () => resolve());
+      this.client.once('error', () => {
+        reject(new Error('Connection not established. Refresh the page'));
+      });
+    });
+  }
+
+  isConnected() {
+    const { OPEN } = Object.getPrototypeOf(this.client.socket);
+    return this.client.socket.readyState === OPEN;
+  }
+
+  close() {
+    this.client.close();
+  }
+
+  register(properties) {
+    return new Promise((resolve, reject) => {
+      if (this.isConnected()) {
+        this.client.register(properties);
+      } else {
+        reject(new Error('Socket closed. Unable to register device. Refresh the page'));
+      }
+      this.client.once('registered', device => resolve(device));
+      this.client.once('error', (err) => {
+        reject(new Error(err));
+      });
+    });
+  }
 }
 
 export default Cloud;
